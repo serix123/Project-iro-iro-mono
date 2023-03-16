44ddfd37f6781302054d79e16c09ce39ec7713de
jakeandrey.suerte <jakeandrey.suerte@awsys-i.com>
2023-03-16 11:19:16 +0800

    Added basic fragments and navigations

--- app/src/main/java/com/jas/androidapp/MainActivity.kt
+++ app/src/main/java/com/jas/androidapp/MainActivity.kt
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.jas.androidapp.core.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +23,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/com/jas/androidapp/core/menu/MenuFragment.kt
@@ -0,0 +1,39 @@
+package com.jas.androidapp.core.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.jas.androidapp.R
+import com.jas.androidapp.databinding.FragmentMenuBinding
+
+
+class MenuFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private lateinit var menuBinding: FragmentMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navController = findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        menuBinding = FragmentMenuBinding.inflate(layoutInflater)
+        menuBinding.textView.setOnClickListener {
+            navController.navigate(R.id.to_dictionary)
+        }
+        return menuBinding.root
+
+    }
+
+    companion object {
+    }
+}
