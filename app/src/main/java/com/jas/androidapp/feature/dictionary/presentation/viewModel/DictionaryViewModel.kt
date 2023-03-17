b3129dc1c9fa3b117621eb21291f740ac8c8adc0
Jake Suerte <jakeandrey.suerte@awsys-i.com>
2023-03-17 12:05:32 +0800

    experiment

--- app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/viewModel/DictionaryViewModel.kt
+++ app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/viewModel/DictionaryViewModel.kt
@@ -8,12 +8,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class DictionaryViewModel: ViewModel() {
-    private val _splashLoading = MutableStateFlow(true )
+    private val _splashLoading = MutableStateFlow(false )
     val splashLoading = _splashLoading.asStateFlow()
     init {
         viewModelScope.launch {
             delay(3000L)
-            _splashLoading.value = false
+            _splashLoading.value = true
         }
     }
 }
