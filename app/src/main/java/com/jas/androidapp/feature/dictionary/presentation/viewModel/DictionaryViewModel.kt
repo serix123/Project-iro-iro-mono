378288050f6a35b049463901ca99c0a005b7229e
Jake Suerte <jakeandrey.suerte@awsys-i.com>
2023-03-17 12:04:29 +0800

    initial branch

--- app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/viewModel/DictionaryViewModel.kt
+++ app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/viewModel/DictionaryViewModel.kt
@@ -1,9 +1,19 @@
 package com.jas.androidapp.feature.dictionary.presentation.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class DictionaryViewModel: ViewModel() {
+    private val _splashLoading = MutableStateFlow(true )
+    val splashLoading = _splashLoading.asStateFlow()
     init {
-
+        viewModelScope.launch {
+            delay(3000L)
+            _splashLoading.value = false
+        }
     }
 }
