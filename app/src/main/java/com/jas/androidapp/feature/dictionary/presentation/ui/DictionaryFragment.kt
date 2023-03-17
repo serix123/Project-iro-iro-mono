16844657b8c09e85d77463f35e5891324668e996
Jake Suerte <jakeandrey.suerte@awsys-i.com>
2023-03-17 11:57:53 +0800

    initial branch

--- app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/ui/DictionaryFragment.kt
+++ app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/ui/DictionaryFragment.kt
@@ -1,15 +1,13 @@
-package com.jas.androidapp.feature.dictionary.presentation
+package com.jas.androidapp.feature.dictionary.presentation.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import com.jas.androidapp.R
 import com.jas.androidapp.databinding.FragmentDictionaryBinding
-import com.jas.androidapp.databinding.FragmentMenuBinding
 
 
 class DictionaryFragment : Fragment() {
---
+++ app/src/main/java/com/jas/androidapp/feature/dictionary/presentation/viewModel/DictionaryViewModel.kt
@@ -0,0 +1,6 @@
+package com.jas.androidapp.feature.dictionary.presentation.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class DictionaryViewModel: ViewModel() {
+}
